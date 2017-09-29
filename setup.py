5a822c60c8683d8a73b3d57716f8fcf6a826e34f
Giuseppe Chiesa <gchiesa@mobiquityinc.com>
2017-09-29 21:18:12 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     zip_safe=False,
     keywords='s3vaultlib',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
