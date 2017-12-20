82f86df31f02da43ddfa7a8492a639a94ad6be31
Giuseppe Chiesa <gchiesa@mobiquityinc.com>
2017-12-20 18:42:47 +0100

    Bump version: 1.6.1 → 1.6.2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='s3vaultlib',
-    version='1.6.1',
+    version='1.6.2',
     description="Python library to expose S3 as vault to store encrypted data",
     long_description=readme + '\n\n' + history,
     author="Giuseppe Chiesa",
