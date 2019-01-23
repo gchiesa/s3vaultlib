dca395670f7a57724f653046cef8faea5ce72739
Giuseppe Chiesa <mail@giuseppechiesa.it>
2019-01-23 21:39:30 +0100

    Bump version: 2.3.1 → 2.3.2

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 
 setup(
     name=__application__,
-    version='2.3.1',
+    version='2.3.2',
     description="Python library to expose S3 as vault to store encrypted data",
     long_description=readme + '\n\n' + history,
     author="Giuseppe Chiesa",
