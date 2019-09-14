c4252ff5654837999c364701bf9ef81a06f794aa
Giuseppe Chiesa <mail@giuseppechiesa.it>
2019-09-14 21:36:26 +0200

    updated tox to support python 3 various versions

--- setup.py
+++ setup.py
@@ -48,6 +48,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     test_suite='tests',
     tests_require=test_requirements,
